e8439ab128a30b4a5e0bafb6df95bd914ffc5208
Yann Mazita <dev.mazita@gmail.com>
2023-10-28 00:02:19 +0200

    Updated main.js vue and axios imports

--- front/src/main.js
+++ front/src/main.js
@@ -2,9 +2,6 @@ import { createApp } from 'vue'
 import axios from 'axios'
 import App from './App.vue'
 
-// Vue.createApp(App).mount('#app')
 const app = createApp(App);
 app.mount('#app')
-// app.config.globalProperties.$http = axios;
-app.config.globalProperties.$axios = axios;
-// Vue.prototype.$http = axios;
+app.config.globalProperties.$http = axios;
