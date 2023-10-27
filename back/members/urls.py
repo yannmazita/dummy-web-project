245ff9d11090ad13361dbc214428ece644f57abb
Yann Mazita <dev.mazita@gmail.com>
2023-10-28 00:42:51 +0200

    Updated API urls

--- back/members/urls.py
+++ back/members/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from members import views
 
 urlpatterns = [
-    path("members/", views.members),
+    path("members.json", views.members),
     path("members/<int:pk>", views.member_detail),
 ]
