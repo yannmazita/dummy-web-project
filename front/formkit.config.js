08738af3ab9ef549fa626ead5e7c400004971d05
Yann Mazita <dev.mazita@gmail.com>
2023-11-05 22:20:17 +0100

    Added multi-step input plugin

--- front/formkit.config.js
+++ front/formkit.config.js
@@ -1,10 +1,13 @@
 import { fr } from '@formkit/i18n'
 import { genesisIcons } from '@formkit/icons'
+import { createMultiStepPlugin } from '@formkit/addons'
+import '@formkit/addons/css/multistep'
 
 const config = {
     icons: {
         genesisIcons,
     },
+    plugins: [createMultiStepPlugin()],
     locales: { fr },
     locale: 'fr',
 }
