741be3468de37d04e26177a708882239989e6fbe
Yann Mazita <dev.mazita@gmail.com>
2023-11-09 20:31:34 +0100

    Update

--- front/src/components/club-space/UpdateAdherentComponent.vue
+++ front/src/components/club-space/UpdateAdherentComponent.vue
@@ -1,35 +1,39 @@
 <template>
-    <main>
+    <FormKit
+        type="form"
+        @submit="submitForm"
+        :config="{ validationVisibility: 'live'}"
+    >
         <FormKit
-            type="form"
-            @submit="submitForm"
-            :config="{ validationVisibility: 'live'}"
-        >
-            <FormKit
-                type="number"
-                name="no_licence"
-                id="no_licence"
-                label="Numéro licence"
-                help="Le numéro de licence FFVB/FSGT."
-                placeholder="1337"
-                validation="min:0|required"
-            />
-        </FormKit>
-    </main>
+            type="number"
+            name="no_licence"
+            id="no_licence"
+            label="Numéro licence"
+            help="Le numéro de licence FFVB/FSGT."
+            placeholder="1337"
+            validation="min:0|required"
+        />
+    </FormKit>
 </template>
 
 
 <script setup>
-    //import AdherentCreation from './AdherentCreation.vue'
     import axios from 'axios'
+    import { ref, watch } from 'vue'
 
-    let adherent = {};
+    const adherent = ref({});
+
+    watch(adherent, function(newAdherent) {
+        if (adherent.value !== newAdherent.value){
+            //pass
+        }
+    })
 
     async function submitForm(fields){
         try {
             const response = await axios.get(`http://localhost:8000/api/adherent/no_licence=${fields.no_licence}`);
-            adherent = response.data;
-            console.log(adherent);
+            adherent.value = response.data;
+            console.log(adherent.value);
         }
         catch (error){
             console.log(error);
