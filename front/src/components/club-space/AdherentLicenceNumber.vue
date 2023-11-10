95d02edd84e4bc4549f55a5fe970eeceba2f5fdb
Yann Mazita <dev.mazita@gmail.com>
2023-11-10 22:23:59 +0100

    Split component in two, tentative data sharing between components

---
+++ front/src/components/club-space/AdherentLicenceNumber.vue
@@ -0,0 +1,53 @@
+<template>
+    <FormKit
+        type="form"
+        @submit="getLicenseNumber"
+        :config="{ validationVisibility: 'live'}"
+        v-if="!control"
+    >
+        <FormKit
+            type="number"
+            name="no_licence"
+            id="no_licence"
+            label="Numéro licence"
+            help="Le numéro de licence FFVB/FSGT."
+            placeholder="1337"
+            validation="min:0|required"
+        />
+    </FormKit>
+</template>
+
+
+<script>
+    /*
+    export default {
+        data(){
+            return{
+                control: false,
+                licenseNumber: -1,
+            }
+        },
+        methods: {
+            getLicenseNumber(fields){
+                this.licenseNumber = fields.no_licence;
+                this.$emit('licenseNumber');
+                console.log(this.licenseNumber);
+            }
+        },
+        emits: ['licenseNumber'],
+    }
+    */
+</script>
+<script setup>
+    import { ref, defineEmits } from 'vue'
+
+    let control = ref(false);
+    let licenseNumber = ref(-1);
+    const emit = defineEmits(['licenseNumber'])
+
+    const getLicenseNumber = function(fields){
+        licenseNumber = fields.no_licence;
+        emit('licenseNumberEntered', licenseNumber);
+        //licenseNumber.value!==-1?control.value=true:control.value=false;
+    }
+</script>
--- front/src/components/club-space/UpdateAdherentComponent.vue
+++ front/src/components/club-space/UpdateAdherentComponent.vue
@@ -1,36 +1,50 @@
 <template>
-    <FormKit
-        type="form"
-        @submit="getLicenceNumber"
-        :config="{ validationVisibility: 'live'}"
-        v-if="!control"
-    >
-        <FormKit
-            type="number"
-            name="no_licence"
-            id="no_licence"
-            label="Numéro licence"
-            help="Le numéro de licence FFVB/FSGT."
-            placeholder="1337"
-            validation="min:0|required"
-        />
-    </FormKit>
+    <AdherentLicenceNumber @licenseNumberEntered="(payload) => {getAdherentFromLicenseNumber(payload);}"/>
+    <button @click="getAdherent" class="btn btn-primary">get adherent</button>
 </template>
 
+<script>
+    /*
+    import AdherentLicenceNumber from './AdherentLicenceNumber.vue'
+    import axios from 'axios'
+    export default {
+        data(){
+            return{
+                adherent: {},
+            }
+        },
+        methods: {
+            async getAdherent(){
+                try {
+                    const response = await axios.get(`http://localhost:8000/api/adherent/no_licence=${this.licenseNumber}`);
+                    this.adherent = response.data;
+                    console.log(this.adherent);
+                    console.log(`licenseNumber in parent=${this.licenseNumber}`);
+                }
+                catch (error){
+                    console.log(`licenseNumber in parent=${this.licenseNumber}`);
+                    console.log(error);
+                }
+            },
+        },
+        props: [
+            'licenseNumber',
+        ]
+    }
+                    console.log(`licenseNumber in parent=${this.licenseNumber}`);
+    */
+</script>
 
 <script setup>
-    //import axios from 'axios'
+    import AdherentLicenceNumber from './AdherentLicenceNumber.vue'
+    import axios from 'axios'
     import { ref } from 'vue'
 
-    //const adherent = ref({});
-    let licenseNumber = ref(-1);
-    let control = ref(false);
+    const adherent = ref({});
 
-
-    /*
-    async function getAdherent(){
+    async function getAdherentFromLicenseNumber(licenseNumber){
         try {
-            const response = await axios.get(`http://localhost:8000/api/adherent/no_licence=${licenseNumber.value}`);
+            const response = await axios.get(`http://localhost:8000/api/adherent/no_licence=${licenseNumber}`);
             adherent.value = response.data;
             console.log(adherent.value);
         }
@@ -38,13 +52,4 @@
             console.log(error);
         }
     }
-    */
-
-    const getLicenceNumber = function(fields){
-        licenseNumber = fields.no_licence;
-        console.log(licenseNumber);
-        licenseNumber.value!==-1?control.value=true:control.value=false;
-    }
-    
-        
 </script>
