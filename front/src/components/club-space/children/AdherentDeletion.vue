cf4e4c6a733876eab34a74ee10b1255356d10965
Yann Mazita <dev.mazita@gmail.com>
2023-11-15 14:38:48 +0100

    Added tailwindcss and daisyui to project

--- front/src/components/club-space/children/AdherentDeletion.vue
+++ front/src/components/club-space/children/AdherentDeletion.vue
@@ -1,67 +1,8 @@
 <template>
-    <div v-if="control">
-    <div>
-        <span>Voulez-vous suppimer l'adhérent {{ lastname }} {{ firstname }} ?</span>
-        <br>
-        <span>Date de naissance: {{ dateOfBirth }}</span>
-    </div>
-    <div>
-        <button @click="deleteAdherent=true" class="btn btn-primary">Oui</button>
-        <button @click="deleteAdherent=false" class="btn btn-primary">Non</button>
-    </div>
-    </div>
+    <div>hello</div>
+    <h1>hello</h1>
+    <h1 className="text-3xl font-bold underline">hello</h1>
 </template>
 
 <script setup>
-    import { ref, defineProps, defineEmits, watch } from 'vue'
-    import axios from 'axios'
-
-    const props = defineProps({
-        adherent:Object
-    })
-
-    let adherent = ref(props.adherent);
-    let firstname = ref(adherent.value.prenom);
-    let lastname = ref(adherent.value.nom);
-    let dateOfBirth = ref(adherent.value.date_naissance);
-
-    
-    let control = ref(false)
-    let deleteAdherent = ref(false);
-    let adherentDeleted = ref(false);
-    const emit = defineEmits(['adherentDeleted'])
-
-    async function deleteAdherentFromDatabase(){
-        try {
-            await axios.delete(`http://localhost:8000/api/adherents/no_licence=${adherent.value.no_licence}`);
-            adherentDeleted.value = true;
-            emit('adherentDeleted', adherentDeleted);
-        }
-        catch (error){
-            console.log(error);
-            adherentDeleted.value = false;
-            emit('adherentDeleted', adherentDeleted);
-        }
-
-    }
-
-    watch(props, (newProps) =>{
-        if (newProps.adherent != null || newProps.adherent != undefined){
-            control.value = true;
-        }
-        else{
-            control.value = false;
-        }
-    })
-
-    watch(deleteAdherent,(newDeleteAdherent) =>{
-        if (newDeleteAdherent){
-            deleteAdherentFromDatabase;
-            adherentDeleted.value = true;
-        }
-        else{
-            control.value = false;
-        }
-    })
-
 </script>
--- front/src/main.js
+++ front/src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import { defaultConfig, plugin } from '@formkit/vue'
 import config from '../formkit.config.js'
 import '@formkit/themes/genesis'
+import './style.css'
 import App from './App.vue'
 
 const app = createApp(App);
