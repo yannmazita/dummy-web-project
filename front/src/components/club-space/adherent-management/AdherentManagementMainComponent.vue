bbe722582977abf9bad3305d2467e3fc13e29d38
Yann Mazita <dev.mazita@gmail.com>
2023-11-15 23:05:52 +0100

    Updated button margins, tweaked flexibility

--- front/src/components/club-space/adherent-management/AdherentManagementMainComponent.vue
+++ front/src/components/club-space/adherent-management/AdherentManagementMainComponent.vue
@@ -1,9 +1,9 @@
 <template>
     <h2>Gestion des adhérents</h2>
-    <div class="flex flex-wrap justify-center">
-        <button @click="components='AddAdherentComponent'" class="btn btn-sm">Ajouter adhérent</button>
-        <button @click="components='UpdateAdherentComponent'" class="btn btn-sm">Mettre à jour adhérent</button>
-        <button @click="components='DeleteAdherentComponent'" class="btn btn-sm">Supprimer adhérent</button>
+    <div class="flex flex-col sm:flex-row justify-center">
+        <button @click="components='AddAdherentComponent'" class="btn mx-3 my-2 sm:m-1 sm:btn-sm">Ajouter adhérent</button>
+        <button @click="components='UpdateAdherentComponent'" class="btn mx-3 my-2 sm:m-1 sm:btn-sm">Mettre à jour adhérent</button>
+        <button @click="components='DeleteAdherentComponent'" class="btn mx-3 my-2 sm:m-1 sm:btn-sm">Supprimer adhérent</button>
     </div>
         <component :is="components"></component>
 </template>
