2d508ac57c80272c7a8e2b52e741edcb073695c7
Yann Mazita <dev.mazita@gmail.com>
2023-11-16 16:34:58 +0100

    Updated navbar z-index

--- front/src/components/NavigationBarComponent.vue
+++ front/src/components/NavigationBarComponent.vue
@@ -2,7 +2,7 @@
 <div class="navbar bg-base-100">
   <div class="navbar-start">
     <div class="dropdown">
-      <label tabindex="0" class="btn btn-ghost lg:hidden">
+      <label tabindex="0" class="btn btn-ghost z-50 lg:hidden">
         <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
       </label>
       <ul tabindex="0" class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
@@ -25,7 +25,7 @@
     <router-link to="/" class="btn btn-ghost text-xl">SCP Volley</router-link>
   </div>
 
-  <div class="navbar-center hidden lg:flex">
+  <div class="navbar-center hidden lg:flex lg:z-50">
     <ul class="menu menu-horizontal px-1">
           <li tabindex="0"><router-link to="/">Accueil</router-link></li>
           <li><router-link to="/agenda">Agenda</router-link></li>
