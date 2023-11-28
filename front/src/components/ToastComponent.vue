0f1166e97fa4dbaaf358cc5e42ee47a7b5fe6d11
dgafip <140158563+dgafip@users.noreply.github.com>
2023-11-28 14:58:53 +0100

    Add audio alert when toast is shown

--- front/src/components/ToastComponent.vue
+++ front/src/components/ToastComponent.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="toast toast-center toast-bottom sm:toast-end sm:toast-top">
+    <div class="toast toast-center toast-bottom sm:toast-end sm:toast-top" role="alert" aria-live="polite" aria-atomic="true" >
         <div class="alert alert-success">
             <span><slot></slot></span>
         </div>
